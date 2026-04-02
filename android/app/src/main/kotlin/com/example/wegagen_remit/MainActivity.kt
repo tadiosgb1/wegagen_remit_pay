1bfc598d9233831dd29d58999cde310e0a28f7a5
Tesfish2012 <tesfay3814@gmail.com>
2026-04-02 16:54:25 +0300

    Remitance Moible app Intailzed

--- android/app/src/main/kotlin/com/example/wegagen_remit/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.wegagen_remit
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- front-end/src/components/configurations/view.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <div>Configurations</div>
-</template>
